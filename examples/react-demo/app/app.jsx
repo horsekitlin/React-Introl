b5669bbc809bb1629d427e1e8515acdc0f983fa9
tomas <horsekit1982@gmail.com>
2015-12-03 19:14:47 +0800

    增加lib

--- examples/react-demo/app/app.jsx
+++ examples/react-demo/app/app.jsx
@@ -2,13 +2,80 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
+var Dog = React.createClass({
+    componentWillMount : function(){
+        alert('招喚神犬!');
+    },
+    componentDidMount : function(){
+        alert('componentDidMount');
+        $('#dog').animate({width : '500px'}, 'slow');
+        $('#dog').animate({height : '500px'}, 'slow');
+        $('#dog').animate({weight : '200px'}, 'slow');
+        $('#dog').animate({height : '200px'}, 'slow');
+    },
+    componentWillUnmount : function(){
+        alert('汪！汪！');
+    },
     render : function(){
+        return <img id='dog' src='http://www.daliulian.net/imgs/image/19/1976926.jpg' />
+    }
+});
+
+var Cat = React.createClass({
+    componentWillMount : function(){
+        alert('招喚神喵!');
+    },
+    componentDidMount : function(){
+        alert('奴才自己滾來見我！');
+    },
+    componentWillUnmount : function(){
+        alert('睡！');
+    },
+    render : function(){
+        return <img src='https://upload.wikimedia.org/wikipedia/commons/a/a2/Cat_Golden_Chinchilla.jpg' />
+    }
+});
+var Main = React.createClass({
+    getInitialState : function(){
+        return {
+            type : ''
+        };
+    },
+    calldog : function(){
+        this.setState({type : 'dog'});
+    },
+    callcat : function(){
+        this.setState({type : 'cat'});
+    },
+    clear : function(){
+        this.setState({type : ''});
+    },
+    render : function(){
+        var display;
+        var type = this.state.type;
+
+        if(type === ''){
+            display = <h3>您尚未招喚寵物</h3>;
+        }else if(type === 'dog'){
+            display = <Dog />;
+        }else if(type === 'cat'){
+            display = <Cat />;
+        }
         return (
-            <p>Hello world!</p>
-        );
+            <div>
+                <button
+                    onClick={this.calldog}>招喚神犬</button>
+                <button
+                    onClick={this.callcat}>招喚神喵</button>
+                <button
+                    onClick={this.clear}>回收</button>
+                <div>
+                    {display}
+                </div>
+            </div>
+        )
     }
 });
 ReactDOM.render(
-    <Hello />,
-      document.getElementById('container'));
+    <Main />,
+    document.getElementById('container'));
